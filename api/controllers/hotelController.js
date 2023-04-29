a428934eaeaf7dacc33b92316f1486467c44439e
Ramzi Bouzaiene <ramzi.bouzaiene@sastec-group.com>
2023-04-29 11:47:11 +0100

    setup hotel controller and routeé

---
+++ api/controllers/hotelController.js
@@ -0,0 +1,67 @@
+import Hotel from "../models/Hotel.js";
+
+// @desc Create New Hotel
+// @route POST /api/hotels
+
+export const createHotel = async (req, res, next) => {
+  const newHotel = new Hotel(req.body);
+
+  try {
+    const savedHotel = await newHotel.save();
+    res.status(200).json(savedHotel);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// @desc Update Hotel
+// @route PUT /api/hotels/:id
+
+export const updateHotel = async (req, res, next) => {
+  try {
+    const updatedHotel = await Hotel.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json(updatedHotel);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// @desc Delete a Hotel
+// @route DELETE /api/hotels/:id
+
+export const deleteHotel = async (req, res, next) => {
+  try {
+    await Hotel.findByIdAndDelete(req.params.id);
+    res.status(200).json("Hotel has benn deleted");
+  } catch (error) {
+    next(error);
+  }
+};
+
+// @desc Get a Single Hotel
+// @route GET /api/hotels
+
+export const getHotel = async (req, res, next) => {
+  try {
+    const hotel = await Hotel.findById(req.params.id);
+    res.status(200).json(hotel);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// @desc Get All Hotels
+// @route GET /api/hotels
+
+export const getAllHotels = async (req, res, next) => {
+  try {
+    const hotels = await Hotel.find();
+    res.status(200).json(hotels);
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,65 +1,22 @@
 import express from "express";
-import Hotel from "../models/Hotel";
+import {
+  createHotel,
+  deleteHotel,
+  getAllHotels,
+  getHotel,
+  updateHotel,
+} from "../controllers/hotelController";
 
 const router = express.Router();
 
-// @desc Create New Hotel
-// @route POST /api/hotels
-router.post("/", async (req, res) => {
-  const newHotel = new Hotel(req.body);
+router.post("/", createHotel);
 
-  try {
-    const savedHotel = await newHotel.save();
-    res.status(200).json(savedHotel);
-  } catch (error) {
-    res.status(500).json(err);
-  }
-});
+router.put("/:id", updateHotel);
 
-// @desc Update Hotel
-// @route PUT /api/hotels/:id
-router.put("/:id", async (req, res) => {
-  try {
-    const updatedHotel = await Hotel.findByIdAndUpdate(
-      req.params.id,
-      { $set: req.body },
-      { new: true }
-    );
-    res.status(200).json(updatedHotel);
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+router.delete("/:id", deleteHotel);
 
-// @desc Delete a Hotel
-// @route DELETE /api/hotels/:id
-router.delete("/:id", async (req, res) => {
-  try {
-    await Hotel.findByIdAndDelete(req.params.id);
-    res.status(200).json("Hotel has benn deleted");
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+router.get("/:id", getHotel);
 
-// @desc Get a Single Hotel
-// @route GET /api/hotels
-router.get("/:id", async (req, res) => {
-  try {
-    const hotel = await Hotel.findById(req.params.id);
-    res.status(200).json(hotel);
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+router.get("/", getAllHotels);
 
-// @desc Get All Hotels
-// @route GET /api/hotels
-router.get("/", async (req, res, next) => {
-  try {
-    const hotels = await Hotel.find();
-    res.status(200).json(hotels);
-  } catch (error) {
-    next(error);
-  }
-});
+export default router;
